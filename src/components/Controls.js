040346e24d98b34991af59527c58709772ad2d04
Chandu J S <app.coder@outlook.in>
2020-03-05 15:09:27 +0400

    controls styled

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -8,11 +8,11 @@ import ReplayImg from '../assets/replay.png'
 function Controls() {
   return (
     <div className="controls">
-      <button className="control">
+      <button disabled className="control">
         <img src={UndoImg} alt="Undo" className="button-icon" />
         <div className="button-label">Undo</div>
       </button>
-      <button className="control">
+      <button disabled className="control">
         <img src={ReplayImg} alt="Replay" className="button-icon" />
         <div className="button-label">Replay</div>
       </button>
@@ -20,7 +20,7 @@ function Controls() {
         <img src={ResetImg} alt="Reset" className="button-icon" />
         <div className="button-label">Reset</div>
       </button>
-      <button className="control">
+      <button disabled className="control">
         <img src={RedoImg} alt="Redo" className="button-icon" />
         <div className="button-label">Redo</div>
       </button>
