5bb8fb8203b1da06dc86ee77fa60d06bded713ca
Chandu J S <app.coder@outlook.in>
2020-03-05 17:07:32 +0400

    tile items numbering & styling WIP

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -2,18 +2,19 @@ import React from 'react'
 
 import './board.css'
 
-const gridItems = () => {
-  const items = []
-  for (let index = 0; index < 4 * 4; index++) {
-    items.push(<div key={index} className="grid-item"></div>)
-  }
-  return items
-}
-
 const Board = () => {
+  const layoutItems = []
+  for (let index = 0; index < 4 * 4; index++) {
+    layoutItems.push(<div key={index} className="grid-item"></div>)
+  }
+
   return (
     <div className="game-board">
-      <div className="board-grid">{gridItems()}</div>
+      <div className="board-grid">{layoutItems}</div>
+      <div className="tile-grid">
+        <div className="tile-item" data-position="0-0" data-value="2"></div>
+        <div className="tile-item" data-position="1-0" data-value="2"></div>
+      </div>
     </div>
   )
 }
