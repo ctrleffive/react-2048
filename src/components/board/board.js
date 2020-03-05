b0501bd911d6393976a73b07b008905766150315
Chandu J S <app.coder@outlook.in>
2020-03-05 23:20:25 +0400

    board number generating methods added

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -1,55 +1,139 @@
 /** @jsx jsx */
+import React from 'react'
 import { css, jsx } from '@emotion/core'
 
 import './board.css'
 
-const Board = () => {
-  const layoutItems = []
-  for (let index = 0; index < 4 * 4; index++) {
-    layoutItems.push(<div key={index} className="grid-item"></div>)
+export default class Board extends React.Component {
+  gridSize
+  dataGrid
+  tileStyles
+  layoutItems
+
+  constructor(props) {
+    super(props)
+    this.gridSize = 4
+    this.dataGrid = []
+    this.tileStyles = ``
+    this.layoutItems = []
+
+    this.state = {
+      dataTiles: []
+    }
+
+    this.prepareGrid()
+    this.prepareTileLayout()
   }
 
-  let tileStyles = ``
-  for (let xIndex = 0; xIndex <= 3; xIndex++) {
-    for (let yIndex = 0; yIndex <= 3; yIndex++) {
-      const xPosition = xIndex * (85 + 10)
-      const yPosition = yIndex * (80 + 10)
-      tileStyles += `
-        .tile-item[data-position="${xIndex}-${yIndex}"] {
-          transform: translate(${xPosition}px, ${yPosition}px);
-        }
-      `
+  prepareGrid() {
+    for (let xIndex = 0; xIndex < this.gridSize; xIndex++) {
+      const row = []
+      for (let yIndex = 0; yIndex < this.gridSize; yIndex++) {
+        row.push(null)
+      }
+      this.dataGrid.push(row)
     }
   }
 
-  return (
-    <div className="game-board">
-      <div className="board-grid">{layoutItems}</div>
-      <div
-        className="tile-grid"
-        css={css`
-          ${tileStyles}
-        `}
-      >
-        <div className="tile-item" data-position="0-0" data-value="2"></div>
-        <div className="tile-item" data-position="1-0" data-value="4"></div>
-        <div className="tile-item" data-position="2-0" data-value="8"></div>
-        <div className="tile-item" data-position="3-0" data-value="16"></div>
-        <div className="tile-item" data-position="0-1" data-value="32"></div>
-        <div className="tile-item" data-position="1-1" data-value="64"></div>
-        <div className="tile-item" data-position="2-1" data-value="2"></div>
-        <div className="tile-item" data-position="3-1" data-value="128"></div>
-        <div className="tile-item" data-position="0-2" data-value="256"></div>
-        <div className="tile-item" data-position="1-2" data-value="512"></div>
-        <div className="tile-item" data-position="2-2" data-value="1024"></div>
-        <div className="tile-item" data-position="3-2" data-value="2048"></div>
-        <div className="tile-item" data-position="0-3" data-value="2"></div>
-        <div className="tile-item" data-position="1-3" data-value="4"></div>
-        <div className="tile-item" data-position="2-3" data-value="8"></div>
-        <div className="tile-item" data-position="3-3" data-value="16"></div>
-      </div>
-    </div>
-  )
-}
+  putNewNumber() {
+    const freeCells = []
+    this.dataGrid.forEach((xItem, x) => {
+      xItem.forEach((yItem, y) => {
+        if (yItem === null) {
+          freeCells.push({
+            position: { x, y },
+            number: Math.round(Math.random()) === 0 ? 2 : 4
+          })
+        }
+      })
+    })
+    if (freeCells.length === 0) {
+      alert('full!')
+    } else {
+      const randomIndex = Math.floor(Math.random() * (freeCells.length - 1))
+      const randomCell = freeCells[randomIndex]
 
-export default Board
+      this.dataGrid[randomCell.position.x][randomCell.position.y] = randomCell
+      const dataTiles = this.state.dataTiles
+      dataTiles.push(
+        <div
+          key={`${randomCell.position.x}${randomCell.position.y}`}
+          className="tile-item"
+          data-position={`${randomCell.position.x}-${randomCell.position.y}`}
+          data-value={randomCell.number}
+        ></div>
+      )
+      this.setState({ dataTiles })
+    }
+  }
+
+  prepareTileLayout() {
+    this.dataGrid.forEach((xItem, xIndex) => {
+      xItem.forEach((_yItem, yIndex) => {
+        const xPosition = xIndex * (85 + 10)
+        const yPosition = yIndex * (80 + 10)
+        this.tileStyles += `
+          .tile-item[data-position="${xIndex}-${yIndex}"] {
+            transform: translate(${xPosition}px, ${yPosition}px);
+          }
+        `
+        this.layoutItems.push(<div key={`${xIndex}${yIndex}`} className="grid-item"></div>)
+      })
+    })
+  }
+
+  keyListener({ board, keyCode }) {
+    switch (keyCode) {
+      // left arrow
+      case 37:
+        board.putNewNumber()
+        break
+
+      // top arrow
+      case 38:
+        console.log('top!')
+        break
+
+      // right arrow
+      case 39:
+        console.log('right!')
+        break
+
+      // down arrow
+      case 40:
+        console.log('down!')
+        break
+
+      default:
+        break
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', ({ keyCode }) => {
+      this.keyListener({ keyCode, board: this })
+    })
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', ({ keyCode }) => {
+      this.keyListener({ keyCode, board: this })
+    })
+  }
+
+  render() {
+    return (
+      <div className="game-board">
+        <div className="board-grid">{this.layoutItems}</div>
+        <div
+          className="tile-grid"
+          css={css`
+            ${this.tileStyles}
+          `}
+        >
+          {this.state.dataTiles}
+        </div>
+      </div>
+    )
+  }
+}
