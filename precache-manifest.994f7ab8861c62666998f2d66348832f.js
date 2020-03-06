1577dd706ebb463770197d3be66158650e742740
Chandu J S <app.coder@outlook.in>
2020-03-06 17:08:47 +0400

    Updates

---
+++ precache-manifest.994f7ab8861c62666998f2d66348832f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f49c4d4ec1c81d19d1858e7de1dc2af",
+    "url": "/react-2048/index.html"
+  },
+  {
+    "revision": "421a155ba2e3262a8977",
+    "url": "/react-2048/static/css/main.55487d4a.chunk.css"
+  },
+  {
+    "revision": "4ff4f5243b590c5cd66b",
+    "url": "/react-2048/static/js/2.cfbd7592.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-2048/static/js/2.cfbd7592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "421a155ba2e3262a8977",
+    "url": "/react-2048/static/js/main.c23458fb.chunk.js"
+  },
+  {
+    "revision": "8cf12031ceb297fa8a22",
+    "url": "/react-2048/static/js/runtime-main.c53768a2.js"
+  }
+]);
